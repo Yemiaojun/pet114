32695cbeafaf2234448d4eebfd2345be7df6bbae
ZenXingyhun <134491420+ZenXingyhun@users.noreply.github.com>
2024-04-03 14:00:26 +0800

    修改上传和删除流程资源文件的逻辑

--- src/main/java/com/example/wechat/service/FileStorageService.java
+++ src/main/java/com/example/wechat/service/FileStorageService.java
@@ -1,5 +1,6 @@
 package com.example.wechat.service;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -58,16 +59,16 @@ public class FileStorageService {
      * 存储存储过程图片到指定目录，并返回存储的文件路径。
      *
      * @param file 要存储的图片文件
-     * @param name 存储文件的名称前缀
+     * @param id 存储文件的id
      * @return 存储的文件路径
      * @throws IOException 如果存储失败或文件为空，则抛出 IOException
      */
-    public String storeProcedurePic(MultipartFile file, String name) throws IOException {
+    public String storeProcedurePic(MultipartFile file, ObjectId id) throws IOException {
         if (file.isEmpty()) {
             throw new IOException("Failed to store empty file.");
         }
 
-        Path destinationPath = Paths.get(procedurePicDir + name + "_" + file.getOriginalFilename());
+        Path destinationPath = Paths.get(procedurePicDir + id.toString() + "_" + file.getOriginalFilename());
         Files.copy(file.getInputStream(), destinationPath);
 
         return destinationPath.toString();
@@ -77,16 +78,16 @@ public class FileStorageService {
      * 存储存储过程视频到指定目录，并返回存储的文件路径。
      *
      * @param file 要存储的视频文件
-     * @param name 存储文件的名称前缀
+     * @param id 存储文件的id
      * @return 存储的文件路径
      * @throws IOException 如果存储失败或文件为空，则抛出 IOException
      */
-    public String storeProcedureVid(MultipartFile file, String name) throws IOException {
+    public String storeProcedureVid(MultipartFile file, ObjectId id) throws IOException {
         if (file.isEmpty()) {
             throw new IOException("Failed to store empty file.");
         }
 
-        Path destinationPath = Paths.get(procedureVidDir + name + "_" + file.getOriginalFilename());
+        Path destinationPath = Paths.get(procedureVidDir + id.toString() + "_" + file.getOriginalFilename());
         Files.copy(file.getInputStream(), destinationPath);
 
         return destinationPath.toString();
@@ -95,12 +96,12 @@ public class FileStorageService {
     /**
      * 删除指定存储过程视频文件。
      *
-     * @param procedureName 存储过程的名称
+     * @param id 存储过程的id
      * @param fileName      要删除的文件名称
      * @throws IOException 如果删除失败或文件不存在，则抛出 IOException
      */
-    public void deleteProcedureVid(String procedureName, String fileName) throws IOException {
-        Path destinationPath = Paths.get(procedureVidDir + procedureName + "_" + fileName);
+    public void deleteProcedureVid(ObjectId id, String fileName) throws IOException {
+        Path destinationPath = Paths.get(procedureVidDir + id.toString() + "_" + fileName);
         if (Files.exists(destinationPath)) {
             Files.delete(destinationPath);
             System.out.println("视频删除成功");
@@ -112,12 +113,12 @@ public class FileStorageService {
     /**
      * 删除指定存储过程图片文件。
      *
-     * @param procedureName 存储过程的名称
+     * @param id 存储过程的id
      * @param fileName      要删除的文件名称
      * @throws IOException 如果删除失败或文件不存在，则抛出 IOException
      */
-    public void deleteProcedurePic(String procedureName, String fileName) throws IOException {
-        Path destinationPath = Paths.get(procedurePicDir + procedureName + "_" + fileName);
+    public void deleteProcedurePic(ObjectId id, String fileName) throws IOException {
+        Path destinationPath = Paths.get(procedurePicDir + id.toString() + "_" + fileName);
         if (Files.exists(destinationPath)) {
             Files.delete(destinationPath);
             System.out.println("图片删除成功");
